50da6148ea57f1568b750c6a0f87bf75e52af157
PaolaMontaño <paolamo979@gmail.com>
2025-04-06 13:19:13 +0200

    actualizacion

--- JS/Projects.js
+++ JS/Projects.js
@@ -1,35 +1,30 @@
 window.addEventListener("load", async () => {
     try {
-        const response = await fetch("https://raw.githubusercontent.com/ironhack-jc/mid-term-api/main/projects");
-        const data = await response.json();
+        const res = await fetch("https://raw.githubusercontent.com/ironhack-jc/mid-term-api/main/projects");
+        const data = await res.json();
 
-        const mainProjectId = getQuerystringId();
-        const mainProjectIndex = data.findIndex(project => project.uuid === mainProjectId);
+        const id = getQuerystringId();
+        const index = data.findIndex(p => p.uuid === id);
+        const [mainProject] = data.splice(index, 1); 
 
-        const mainProject = data[mainProjectIndex];
         addMainProject(mainProject);
-
-        data.splice(mainProjectIndex, 1);
-        const otherProjects = data.slice(0, 3).reverse();
-        addOtherProjects(otherProjects);
-    } 
-    catch (error) {
+        addOtherProjects(data.slice(0, 3).reverse());
+    } catch (err) {
         toggleModal();
     }
 });
 
 function getQuerystringId() {
-    const params = new Proxy(new URLSearchParams(window.location.search), {
-        get: (searchParams, prop) => searchParams.get(prop),
-    });
-    return params.id;
+    return new Proxy(new URLSearchParams(location.search), {
+        get: (params, prop) => params.get(prop)
+    }).id;
 }
 
 function addMainProject(project) {
     if (!project) return;
 
     const { name, description, completed_on, image, content } = project;
-    const projectHTML = `
+    document.getElementById("project").innerHTML = `
         <h1 class="title">${name}</h1>
         <div class="subtitle">
             <span class="UI-design-title">${description}</span>
@@ -42,26 +37,22 @@ function addMainProject(project) {
         </div>
         <article class="project-description">${content}</article>
     `;
-
-    document.getElementById("project").innerHTML = projectHTML;
 }
 
 function addOtherProjects(projects) {
-    const container = document.querySelector("div.projects-container");
-    container.innerHTML = projects.map(jsonProjectToOtherHtmlArticle).join("");
+    document.querySelector(".projects-container").innerHTML =
+        projects.map(jsonProjectToOtherHtmlArticle).join("");
 }
 
 function jsonProjectToOtherHtmlArticle({ uuid, name, image, description }) {
-    if (!uuid || !name || !image || !description) return "";
-    
     return `
         <article class="project-card">
-            <a class="project-wrapper" href="../pages/projects.html?id=${uuid}">
+            <a class="project-wrapper" href="projects.html?id=${uuid}">
                 <img class="img-project" src="${image}" alt="${name} image" />
                 <div class="project-inner-card">
                     <h4 class="project-title">${name}</h4>
                     <p class="project-description capitalize">${description}</p>
-                    <a class="learn-more" href="../pages/projects.html?id=${uuid}">Learn more</a>
+                    <a class="learn-more" href="projects.html?id=${uuid}">Learn more</a>
                 </div>
             </a>
         </article>
--- README.md
+++ README.md
@@ -1,21 +1,76 @@
-Project IronHack
+# **Proyecto IronHack
 
-Technologies used:
-HTML5
-CSS3
-JAVASCRIPT
-GITHUB
-VISUAL STUDIO CODE
-GIT
-NETLIFY
+El proyecto está basado en el diseño proporcionado en este archivo de Figma: 
 
-Responsiveness Page
-The main page (Home.html) has been made responsive by means of the Home.css page (@media (max-width: 950px)), where the content adapts depending on the width of the screen.
+https://www.figma.com/file/fO1DtHTp1oVIV03V4o0kMx/mid-term-project?type=design&node-id=0-1&mode=design&t=1MhBfRFdA3Z6wwR6-0
 
-The endpoint was utilized to retrieve data through an asynchronous function (fetch) in order to dynamically inject it into the required fields on the projects page (projects.html) using JavaScript and the DOM. (Document Object Model).
+---
 
-Link to the deployment URL and link to the presentation.
-Netlify: 
-Presentation: 
-Repository GitHub:
+## Enlace a la URL de implementación y presentación.
 
+Netlify: (https://project1-circle-paomont.netlify.app/)
+Repositorio en GitHub: (https://github.com/979-Pao/Project_1_Circle)
+Presentación: ()
+
+---
+
+## Tecnologías utilizadas:
+
+HTML: Estructura del contenido.
+
+CSS: Estilos, incluyendo las MediaQueries responsive.
+
+JavaScript: Para funcionalidades como la validacion del formulario contact y el boton Scroll, recuperacion datos mediante fetch, para introduccion a los campos de proyectos.
+
+---
+
+## Contenido del sitio web
+
+Sitio web para una empresa ficticia de diseño web. Incluye tres páginas:
+
+Página de inicio
+Página de proyectos
+Página de contacto con un formulario
+
+---
+
+## Funcionalidades HTML / CSS / JS
+
+Estructura (HTML) y estilos (CSS) de las tres páginas siguiendo el diseño de Figma.
+
+Diseño responsive para dispositivos móviles y tablets usando media queries (CSS) para las tres páginas.
+
+Encabezado (Barra navegacion) y pie de página (Footer) comunes para todas las páginas en Global.css. El encabezado de cada HTML, para evitar la repeticion de estilo en css.
+
+En el formulario de contacto en respuesta a la validación: resaltar o desresaltar campos incorrectos y limpiar datos tras un envío exitoso
+
+El endpoint se utilizó para recuperar datos mediante una función asíncrona (fetch) e inyectarlos dinámicamente en los campos requeridos de la página del proyecto (projects.html) mediante JavaScript y el DOM (Modelo de Objetos del Documento).
+
+--- 
+
+## Diseño responsive
+
+width 760px, tablets
+width 500px, mobile
+
+---
+
+## Estructura del proyecto
+
+Carpetas:
+
+HOME (home.html) (página de inicio): archivos HTML, CSS y JS de las paginas están dentro de subcarpetas
+
+Carpeta Img: para guardar imágenes y otros recursos
+
+Carpeta PAGES: Contact_us y Projects
+
+Carpeta CSS: Global, Home, Contact_us y Projects
+
+Carpeta JS: Home, Contact_us y Projects
+
+---
+
+Paola Montaño.
+
+---
